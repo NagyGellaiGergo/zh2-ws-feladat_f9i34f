4317dab069c359e0e37edc78367adcb859d5fb44
NagyGellaiGergo <nggeriii@gmail.com>
2025-05-07 17:28:39 +0200

    2 feladat
    2. feladat

--- src/main/java/hu/pte/mik/prog4/zh2/ws/FoodDataService.java
+++ src/main/java/hu/pte/mik/prog4/zh2/ws/FoodDataService.java
@@ -1,7 +1,10 @@
 package hu.pte.mik.prog4.zh2.ws;
 
+import jakarta.jws.WebMethod;
+
 public interface FoodDataService {
 
+    @WebMethod
     FoodDataResponse getFoodData(FoodDataRequest request);
 
 }
